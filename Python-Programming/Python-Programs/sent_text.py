766b9734819eff70c8fcc5a7ed14943929dc3e32
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-22 23:32:56 -0500

    Sent MSN Module

---
+++ Python-Programming/Python-Programs/sent_text.py
@@ -0,0 +1,11 @@
+from twilio.rest import TwilioRestClient
+ 
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC32a3c49700934481addd5ce1659f04d2"
+auth_token  = "{{ auth_token }}"
+client = TwilioRestClient(account_sid, auth_token)
+ 
+message = client.messages.create(body="Jenny please?! I love you <3",
+    to="+14159352345",    # Replace with your phone number
+    from_="+14158141829") # Replace with your Twilio number
+print message.sid
