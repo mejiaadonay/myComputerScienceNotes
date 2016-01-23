e16e7bdf0198287a22b881776f5cb8fe1d3b5710
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-22 23:52:02 -0500

    Add SID

--- Python-Programming/Python-Programs/sent_text.py
+++ Python-Programming/Python-Programs/sent_text.py
@@ -5,7 +5,7 @@ from twilio.rest import TwilioRestClient
 # Date:   1/22/2016
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC32a3c49700934481addd5ce1659f04d2"
+account_sid = "AP9adea64fa812be6ba959b6cf11444949"
 auth_token  = "{{ auth_token }}"
 client = TwilioRestClient(account_sid, auth_token)
  
