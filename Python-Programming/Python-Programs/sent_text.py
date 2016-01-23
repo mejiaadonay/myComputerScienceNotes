fa4e4350ea8818e32aba102a6471eb1d6e5b6a23
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-23 00:15:23 -0500

    Add Number

--- Python-Programming/Python-Programs/sent_text.py
+++ Python-Programming/Python-Programs/sent_text.py
@@ -9,7 +9,7 @@ account_sid = "AC115149ed615980cdb9ac130cd854855a"
 auth_token  = "c13a53097d893bb1021e156f52cc955f"
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.messages.create(body="Jenny please?! I love you <3",
-    to="+14159352345",    # Replace with your phone number
-    from_="+14158141829") # Replace with your Twilio number
+message = client.messages.create(body="Gricelda! I love you <3",
+    to="+12025006492",    # Replace with your phone number
+    from_="+14155992671") # Replace with your Twilio number
 print message.sid
