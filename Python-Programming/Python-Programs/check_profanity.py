104941b14dd4ecd664c7acddf03a46e4bb7794c9
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-25 00:39:15 -0500

    Absolute Path

--- Python-Programming/Python-Programs/check_profanity.py
+++ Python-Programming/Python-Programs/check_profanity.py
@@ -4,9 +4,9 @@ def read_text():
 # Author:   Jose Escobar Mejia
 # Date:     01/23/2016
 
-    quotes = open(/Python-Programs/movies_quotes.txt)
+    quotes = open("/Users/adonay/desktop/GitPortfolio/myComputerScienceCourseNotes/Python-Programming/Python-Programs/movies_quotes.txt")
     contents_of_file = quotes.read()
-    print(content_of_file)
+    print(contents_of_file)
     quotes.close()
 
 read_text()
