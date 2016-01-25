90f904366660f990e380a413510a61dc8f9aa128
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-24 23:50:43 -0500

    Open Data File

--- Python-Programming/Python-Programs/check_profanity.py
+++ Python-Programming/Python-Programs/check_profanity.py
@@ -1,4 +1,13 @@
-
+def read_text():
+    
 # check profanity words
 # Author:   Jose Escobar Mejia
 # Date:     01/23/2016
+
+    quotes = open(/Python-Programs/movies_quotes.txt)
+    contents_of_file = quotes.read()
+    print(content_of_file)
+    quotes.close()
+
+read_text()
+    
