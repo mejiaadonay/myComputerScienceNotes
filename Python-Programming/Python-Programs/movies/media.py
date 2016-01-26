720b204d723de430cabd5d5299a9ecbff65bc388
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-26 01:36:21 -0500

    Valid Rate

--- Python-Programming/Python-Programs/movies/media.py
+++ Python-Programming/Python-Programs/movies/media.py
@@ -6,6 +6,8 @@ import webbrowser
 
 # class definition
 class Movie():
+    valid_ratings = ["G", "PG", "PG-13", "R"]
+    
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
