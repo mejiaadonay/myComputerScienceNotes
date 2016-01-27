597095ebf32ea33ee3d4d3acfa5599270b20135a
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-27 02:03:59 -0500

    def show info Method

--- Python-Programming/Python-Programs/inheritance.py
+++ Python-Programming/Python-Programs/inheritance.py
@@ -18,11 +18,19 @@ class Child(Parent):
         print("Child constructor Called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+        print("Number of toys - " + str(self.number_of_toys))
+    
     
 jose_escobar = Parent("Escobar", "brown")
 #print(jose_escobar.last_name)
-jose_escobar.show_info()
+#jose_escobar.show_info()
 
-#adonay_mejia = Child("Mejia", "brown", 5)
+adonay_mejia = Child("Mejia", "brown", 5)
+adonay_mejia.show_info()
+
 #print(adonay_mejia.last_name)
 #print(adonay_mejia.number_of_toys)
