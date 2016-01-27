ee76e5c2fb56a29cc5f91306a0290379dd1db2c1
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-27 01:57:24 -0500

    Inheritance Class

--- Python-Programming/Python-Programs/inheritance.py
+++ Python-Programming/Python-Programs/inheritance.py
@@ -9,6 +9,10 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+    
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor Called")
@@ -16,8 +20,9 @@ class Child(Parent):
         self.number_of_toys = number_of_toys
     
 jose_escobar = Parent("Escobar", "brown")
-print(jose_escobar.last_name)
+#print(jose_escobar.last_name)
+jose_escobar.show_info()
 
-adonay_mejia = Child("Mejia", "brown", 5)
-print(adonay_mejia.last_name)
-print(adonay_mejia.number_of_toys)
+#adonay_mejia = Child("Mejia", "brown", 5)
+#print(adonay_mejia.last_name)
+#print(adonay_mejia.number_of_toys)
