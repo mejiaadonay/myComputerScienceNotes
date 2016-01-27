dbc848132adba0c324c5ecf32b6387150e9b9fee
Adonay Mejia <mejiaadonay@gmail.com>
2016-01-27 01:28:52 -0500

    Parent Constructor or __init__ function

--- Python-Programming/Python-Programs/inheritance.py
+++ Python-Programming/Python-Programs/inheritance.py
@@ -4,3 +4,9 @@
 # Date:     01/27/2016
 
 class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    
